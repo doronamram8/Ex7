0714fee5b2832f2d91bd763ceab2a572f39175f0
doroanamram8 <amramdoron@gmail.com>
2017-11-24 11:32:23 +0200

    doron

--- app/src/main/java/com/example/madaim/ex7/MainActivity.java
+++ app/src/main/java/com/example/madaim/ex7/MainActivity.java
@@ -1,13 +1,53 @@
 package com.example.madaim.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    final int registercode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_main);
+
+
     }
-}
+
+    public void goToActivity2(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivityForResult(intent, registercode);
+
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == registercode  && resultCode==RESULT_OK) {
+            String gender;
+            Resources res = getResources();
+
+            if (data.getStringExtra("gender").compareTo("Male") == 0) {
+                gender = "Mr. ";
+            }
+                else
+                {
+                    gender = "Ms. ";
+                }
+                String returnString = getString(R.string.back)+" " + gender + " " + data.getStringExtra("firstName") + "," + data.getStringExtra("lastName");
+                TextView textView = (TextView) findViewById(R.id.intro);
+                Button b = (Button) findViewById(R.id.button);
+                b.setText(R.string.bt2);
+                textView.setText(returnString);
+
+            }
+
+        }
+    }
--- app/src/main/java/com/example/madaim/ex7/RegisterActivity.java
+++ app/src/main/java/com/example/madaim/ex7/RegisterActivity.java
@@ -1,7 +1,12 @@
 package com.example.madaim.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class RegisterActivity extends Activity {
 
@@ -10,4 +15,27 @@ public class RegisterActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+    public void onButtonClick(View view) {
+
+        EditText firstname=   (EditText) findViewById(R.id.ed);
+        EditText lastname=   (EditText) findViewById(R.id.ed2);
+        RadioGroup rg= (RadioGroup) findViewById(R.id.radioGroup);
+        int gender= rg.getCheckedRadioButtonId();
+        RadioButton rb=(RadioButton) findViewById(gender);
+        String genderS=rb.getText().toString();
+        String striingToPassBack=firstname.getText().toString();
+        String striingToPassBack2=lastname.getText().toString();
+        Intent intent=new Intent();
+        intent.putExtra("firstName",striingToPassBack);
+        intent.putExtra("lastName",striingToPassBack2);
+        intent.putExtra("gender",genderS);
+        setResult(RESULT_OK,intent);
+        finish();
+
+
+
+
+    }
+
+
 }
